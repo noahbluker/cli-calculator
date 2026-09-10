5faecb0e57b9dbe9a23e5e69d2e4ab4586703b4a
noahbluker <240700680+noahbluker@users.noreply.github.com>
2026-09-10 05:51:41 -0700

    Refactor main function and input handling

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,64 @@
 use std::io::{self, Write};
 
-fn main()
-{
-    let number1 = read_number(" Number 1");
-    let operator = read_operator(" Operator (+, -, *, /, %)");
-    let number2 = read_number(" Number 2");
+fn read_line_or_eof(txt: &str) -> Option<String> {
+  print!("{}: ", txt);
+  io::stdout().flush().expect("Flush error");
 
-    let result = match operator {
-        '+' => number1 + number2,
-        '-' => number1 - number2,
-        '*' => number1 * number2,
-        '/' => {
-            if number2 == 0.0 {
-                println!(" Error: Division by 0");
-                return;
-            }
-            number1 / number2
-        },
-        '%' => {
-            if number2 == 0.0 {
-                println!(" Error: Division by 0");
-                return;
-            }
-            number1 % number2
-        },
-        _ => unreachable!(),
-    };
-
-    println!(" Result: {} {} {} = {}", number1, operator, number2, result);
+  let mut input = String::new();
+  match io::stdin().read_line(&mut input).expect("Read error") {
+    0 => None,
+    _ => Some(input),
+  }
 }
 
-fn read_number(txt: &str) -> f64 {
-    loop {
-        print!("{}: ", txt);
-        io::stdout().flush().expect("Flush error");
-        
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Read error");
+fn read_num(txt: &str) -> Option<f64> {
+  loop {
+    let input = read_line_or_eof(txt)?;
 
-        match input.trim().parse::<f64>() {
-            Ok(num) => return num,
-            Err(_) => println!(" Enter a number"),
-        }
+    match input.trim().parse::<f64>() {
+      Ok(num) => return Some(num),
+      Err(_) => println!(" Enter a number"),
     }
+  }
 }
 
-fn read_operator(txt: &str) -> char {
-    loop {
-        print!("{}: ", txt);
-        io::stdout().flush().expect("Flush error");
+fn read_op(txt: &str) -> Option<char> {
+  loop {
+    let input = read_line_or_eof(txt)?;
+    let input = input.trim();
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Read error");
-
-        let input = input.trim();
-
-        match input {
-            "+" | "-" | "*" | "/" | "%" => return input.chars().next().unwrap(),
-            _ => println!(" Invalid operator: '{}'", input),
-        }
+    match input {
+      "+" | "-" | "*" | "/" | "%" => return input.chars().next(),
+      _ => println!(" Invalid operator: '{}'", input),
     }
+  }
+}
+
+fn main() {
+  let Some(num1) = read_num(" Number 1") else { return println!(); };
+  let Some(op) = read_op(" Operator (+, -, *, /, %)") else { return println!(); };
+  let Some(num2) = read_num(" Number 2") else { return println!(); };
+
+  let result = match op {
+    '+' => num1 + num2,
+    '-' => num1 - num2,
+    '*' => num1 * num2,
+    '/' => {
+      if num2 == 0.0 {
+        println!(" Error: Division by 0");
+        return;
+      }
+      num1 / num2
+    },
+    '%' => {
+      if num2 == 0.0 {
+        println!(" Error: Division by 0");
+        return;
+      }
+      num1 % num2
+    },
+    _ => unreachable!(),
+  };
+
+  println!(" Result: {} {} {} = {}", num1, op, num2, result);
 }
